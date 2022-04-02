cf85315d20815a9dc03fe653e1962efdce975539
Vitor Gouveia <vitorneves.gouveia10@gmail.com>
2022-04-02 20:39:50 -0300

    feat: implement characted movement with sprint and crouch

--- index.js
+++ index.js
@@ -19,24 +19,73 @@ const app = () => {
    */
   const context = canvas.getContext("2d");
 
+  class Sprite {
+    position = {
+      x: 0,
+      y: 0,
+    };
+
+    constructor({ position: { y, x }, image }) {
+      this.position = {
+        x,
+        y,
+      };
+      this.image = new Image();
+      this.image.src = image;
+    }
+
+    // integration of the sprite with canvas
+    render() {
+      context.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }
+
+  const background = new Sprite({
+    position: {
+      x: -735,
+      y: -600,
+    },
+    image: "./src/assets/tiled/Pellet-Town.png",
+  });
+
   canvas.width = 1024;
   canvas.height = 576;
 
   context.fillStyle = "white";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  const image = new Image();
-  image.src = "./src/assets/tiled/Pellet-Town.png";
-
-  image.onload = () => {
-    context.drawImage(image, -735, -600);
-  };
-
   const playerImage = new Image();
   playerImage.src = "./src/assets/images/playerDown.png";
 
-  playerImage.onload = () => {
-    // @ts-ignore
+  const player = new Sprite({
+    position: {
+      x: canvas.width / 2 - playerImage.width / 4 / 2,
+      y: canvas.height / 2 - playerImage.height / 2,
+    },
+    image: "./src/assets/images/playerDown.png",
+  });
+
+  let lastKey = "";
+  let sprint = false;
+  let crouching = false;
+  const keys = {
+    ArrowUp: { pressed: false },
+    ArrowLeft: { pressed: false },
+    ArrowRight: { pressed: false },
+    ArrowDown: { pressed: false },
+
+    a: { pressed: false },
+    w: { pressed: false },
+    d: { pressed: false },
+    s: { pressed: false },
+  };
+
+  const animate = () => {
+    // find way to handle FPS here
+    window.requestAnimationFrame(animate);
+
+    background.render();
+    // player.render()
     context.drawImage(
       playerImage,
       0,
@@ -48,7 +97,81 @@ const app = () => {
       playerImage.width / 4,
       playerImage.height
     );
+
+    let step = 3;
+
+    if (crouching) step /= 2;
+    if (sprint) step *= 2;
+
+    if (
+      (keys.w.pressed && lastKey === "w") ||
+      (keys.ArrowUp.pressed && lastKey === "ArrowUp")
+    ) {
+      background.position.y += step;
+    }
+
+    if (
+      (keys.d.pressed && lastKey === "d") ||
+      (keys.ArrowRight.pressed && lastKey === "ArrowRight")
+    ) {
+      background.position.x -= step;
+    }
+
+    if (
+      (keys.s.pressed && lastKey === "s") ||
+      (keys.ArrowDown.pressed && lastKey === "ArrowDown")
+    ) {
+      background.position.y -= step;
+    }
+
+    if (
+      (keys.a.pressed && lastKey === "a") ||
+      (keys.ArrowLeft.pressed && lastKey === "ArrowLeft")
+    ) {
+      background.position.x += step;
+    }
   };
+
+  animate();
+
+  window.addEventListener("keydown", ({ key, code }) => {
+    if (code.includes("Shift")) {
+      crouching = true;
+      return;
+    }
+
+    if (code === "Space") {
+      sprint = true;
+      return;
+    }
+
+    if (keys[key] === undefined) {
+      // alert("didnt code this key yet!");
+      return;
+    }
+
+    lastKey = key;
+    keys[key].pressed = true; // ?
+  });
+
+  window.addEventListener("keyup", ({ key, code }) => {
+    if (code.includes("Shift")) {
+      crouching = false;
+      return;
+    }
+
+    if (code === "Space") {
+      sprint = false;
+      return;
+    }
+
+    if (keys[key] === undefined) {
+      // alert("didnt code this key yet!");
+      return;
+    }
+
+    keys[key].pressed = false; // ?
+  });
 };
 
 document.addEventListener("DOMContentLoaded", app);
